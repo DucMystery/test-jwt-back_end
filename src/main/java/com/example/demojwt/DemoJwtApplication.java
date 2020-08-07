f7046f0e8454c36e3e6a6b56ae46cef30bf557c1
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-08-07 15:26:16 +0700

    Test jwt thanh cong

---
+++ src/main/java/com/example/demojwt/DemoJwtApplication.java
@@ -0,0 +1,34 @@
+package com.example.demojwt;
+
+import com.example.demojwt.user.User;
+import com.example.demojwt.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class DemoJwtApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJwtApplication.class, args);
+    }
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Khi chương trình chạy
+        // Insert vào csdl một user.
+        User user = new User();
+        user.setUsername("loda");
+        user.setPassword(passwordEncoder.encode("loda"));
+        userRepository.save(user);
+        System.out.println(user);
+    }
+}
